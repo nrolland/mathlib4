666c8866e6ec93432239490c6c2d9b548d57eeb7
nicolas <nrolland@users.noreply.github.com>
2024-08-09 12:36:18 +0200

    .

--- Mathlib/CategoryTheory/Distributors/Basic.lean
+++ Mathlib/CategoryTheory/Distributors/Basic.lean
@@ -9,6 +9,7 @@ import Mathlib.CategoryTheory.Types
 import Mathlib.CategoryTheory.Limits.Types
 import Mathlib.CategoryTheory.Monoidal.Category
 import Mathlib.CategoryTheory.Monoidal.Types.Basic
+import Mathlib.CategoryTheory.Products.Associator
 
 /-!
 # Distributors
@@ -29,19 +30,37 @@ Distributors generalize functors like relations generalizes functions
 
 universe v u
 namespace CategoryTheory
-
+set_option linter.longLine false
 
 variable (A B C D: Cat)
-
+variable (α β γ)
 
 abbrev Dist := Dᵒᵖ × C ⥤ Type
+
+namespace hide
+
+inductive Vector (α : Type u) : Nat → Type u
+  | nil  : Vector α 0
+  | cons : α → {n : Nat} → Vector α n → Vector α (n+1)
+  deriving Repr
+
+namespace Vector
+
+def map2 (f : α → β → γ) : {n : Nat} → Vector α n → Vector β n → Vector γ n
+  | 0,   nil,       nil       => nil
+  | n+1, cons a as, cons b bs => cons (f a b) (map2 f as bs)
+
+end Vector
+
+end hide
 
 
 def Prodprod : Type × Type ⥤ Type  := MonoidalCategory.tensor Type
 
 def proasdd (P : Dist A B) (Q: Dist B C) : Cᵒᵖ × C ⥤ Dist A C  :=
-  let PtQ := Functor.prod P Q ⋙ MonoidalCategory.tensor Type
-  --let PtQ' := sorry
+  let PtimesQ  := Functor.prod P Q ⋙ MonoidalCategory.tensor Type
+  let plug : (Bᵒᵖ × ↑B) × (Cᵒᵖ× A) ⥤  (Bᵒᵖ × A) × (Cᵒᵖ × B) :=  sorry -- by coherence
+  let PtimesQ' := plug ⋙ PtimesQ
   sorry
 
 
