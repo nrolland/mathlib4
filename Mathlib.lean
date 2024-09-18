9c6f4b552ca67a4072fae19dff65f7b944f38b67
Antoine Chambert-Loir <antoine.chambert-loir@u-paris.fr>
2024-09-18 07:39:09 +0000

    feat: lemmas on permutations, cycles, etc. (#9602)
    This PR establishes various lemmas in the theory of permutation groups,
    pertaining to the supports, cycle decompositions, and the conjugation action
    of `ConjAct (Equiv.Perm α)` on `Equiv.Perm α`
    
    This lemmas stem out PR #9359 and are used there to compute the cardinalities
    of the conjugacy classes of permutations.
    
    
    
    Co-authored-by: leanprover-community-mathlib4-bot <leanprover-community-mathlib4-bot@users.noreply.github.com>
    Co-authored-by: Antoine Chambert-Loir <antoine.chambert-loir@math.univ-paris-diderot.fr>
    Co-authored-by: Riccardo Brasca <riccardo.brasca@gmail.com>

--- Mathlib.lean
+++ Mathlib.lean
@@ -2831,6 +2831,7 @@ import Mathlib.GroupTheory.PGroup
 import Mathlib.GroupTheory.Perm.Basic
 import Mathlib.GroupTheory.Perm.Closure
 import Mathlib.GroupTheory.Perm.ClosureSwap
+import Mathlib.GroupTheory.Perm.ConjAct
 import Mathlib.GroupTheory.Perm.Cycle.Basic
 import Mathlib.GroupTheory.Perm.Cycle.Concrete
 import Mathlib.GroupTheory.Perm.Cycle.Factors
