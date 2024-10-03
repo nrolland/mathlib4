57afc7fa2d40f6623327af31d588dca66d08ca8c
Andrew Yang <the.erd.one@gmail.com>
2024-10-03 17:34:25 +0000

    feat(RingTheory/Unramified): Classification of unramifield field extensions. (#15123)
    Co-authored-by: Yury G. Kudryashov <urkud@urkud.name>

--- Mathlib.lean
+++ Mathlib.lean
@@ -4090,6 +4090,7 @@ import Mathlib.RingTheory.TwoSidedIdeal.Operations
 import Mathlib.RingTheory.UniqueFactorizationDomain
 import Mathlib.RingTheory.Unramified.Basic
 import Mathlib.RingTheory.Unramified.Derivations
+import Mathlib.RingTheory.Unramified.Field
 import Mathlib.RingTheory.Unramified.Finite
 import Mathlib.RingTheory.Valuation.AlgebraInstances
 import Mathlib.RingTheory.Valuation.Basic
