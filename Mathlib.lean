db1e3b57111378a54a9782bfeecb2f3fcfdc40ae
Violeta Hernández <vi.hdz.p@gmail.com>
2024-10-09 05:25:01 +0000

    refactor(SetTheory/Ordinal/Enum): better definition for `Ordinal.enumOrd` (#16962)
    The old definition was a hassle to work with, to the extent we first had to prove its equivalence to the current definition. This can be sidestepped thanks to the termination checker. This also has the benefit of avoiding `Ordinal.blsub`, which I'm planning to deprecate in the near future.
    
    There's some lemmas that have to be outright removed rather than deprecated (since it'd be difficult to reprove them), but hopefully all of them have a clear case for being internal workings that should have been `private` to begin with.
    
    Other improvements:
    - We move all of this to its own file, since it's not really needed within `Ordinal/Arithmetic`. 
    - We also replace `Set.Unbounded (· < ·)` with the more canonical `¬ BddAbove` throughout.

--- Mathlib.lean
+++ Mathlib.lean
@@ -4166,6 +4166,7 @@ import Mathlib.SetTheory.Lists
 import Mathlib.SetTheory.Ordinal.Arithmetic
 import Mathlib.SetTheory.Ordinal.Basic
 import Mathlib.SetTheory.Ordinal.CantorNormalForm
+import Mathlib.SetTheory.Ordinal.Enum
 import Mathlib.SetTheory.Ordinal.Exponential
 import Mathlib.SetTheory.Ordinal.FixedPoint
 import Mathlib.SetTheory.Ordinal.FixedPointApproximants
